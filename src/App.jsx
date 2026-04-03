a738a04593c24072152fc82fdd4e3c6a12f50a8b
AARTHI <aarthi26042003@gmail.com>
2026-04-03 11:51:44 +0530

    Fix HashRouter, remove duplicate BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "@clerk/clerk-react";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
